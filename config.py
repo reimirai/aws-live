cb691af21271d6f0dcec69d62ef9af8795ced6a4
reimirai <167175405+reimirai@users.noreply.github.com>
2024-04-16 15:47:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c9qq2isiqi6o.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "limkhawin-employee"
+customregion = "us-east-1"
+
